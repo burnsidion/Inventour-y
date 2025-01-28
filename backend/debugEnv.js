452249dd2422435b06cf11e3491c29847561c36d
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-01-28 14:34:34 -0700

    setting up authorization for routes

--- backend/debugEnv.js
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL || 'Not loaded');
-console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY || 'Not loaded');
