39a1de24dd1602ded5eddaa23d0e9c636da71668
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-01-29 13:00:22 -0700

    Implemented authentication state management with Pinia and local storage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { useAuthStore } from './stores/auth';
+
+const authStore = useAuthStore();
+
+authStore.loadUserFromStorage();
 </script>
 
 <template>
   <div id="app">
     <header>
       <nav>
+        <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/signup">Sign Up</RouterLink>
       </nav>
     </header>
